cf4d15b4ace7c7bca0813322b758a6999a3dcc9a
Léo Baccialone <leo.baccialone@laposte.net>
2022-04-21 19:19:00 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import "./App.css";
 const numbers = [1, 2, 3, 4, 5];
 
 const App = () => {
-  const [choice, setChoice] = useState("?");
+  const [choice, setChoice] = useState(1); // envoyer des infos vers le composant enfant Number
 
-  var handleClick = (num) => {
+  var onClick = (num) => {
     setChoice(num);
   };
 
@@ -19,7 +19,7 @@ const App = () => {
         <h1>Choisir un nombre...</h1>
         <div className="choice">
           {numbers.map((num) => (
-            <Choice key={num} num={num} handleClickParent={handleClick} />
+            <Choice key={num} num={num} changeNumber={onClick} />
           ))}
         </div>
       </section>
--- src/components/Choice.js
+++ src/components/Choice.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const Choice = (props) => {
-  var handleClick = () => {
-    props.handleClickParent(props.num);
+  var onClick = () => {
+    props.changeNumber(props.num); // envoyer des infos vers le composant parent App
   };
 
   return (
-    <span className="number num-choice" onClick={() => handleClick()}>
+    <span className="number num-choice" onClick={() => onClick()}>
       {props.num}
     </span>
   );
